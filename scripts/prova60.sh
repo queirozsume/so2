cbdb2c34b02b3eb038ed99cad122fd0a2ca1fdc8
JOEL QUEIROZ <joelcfq@yandex.ru>
2023-09-25 09:53:57 -0300

    Add files via upload

---
+++ scripts/prova60.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /home/aluno
+touch resumo.txt
+cat /etc/os-release >> resumo.txt
+echo "---------------------------------------------------------" >> resumo.txt
+uname -a >> resumo.txt
+echo "---------------------------------------------------------" >> resumo.txt
+echo $XDG_CURRENT_DESKTOP >> resumo.txt
+echo "---------------------------------------------------------" >> resumo.txt
+cat /etc/apt/sources.list >> resumo.txt
+echo "---------------------------------------------------------" >> resumo.txt
+sudo cat /var/log/dpkg.log >> resumo.txt
+echo "---------------------------------------------------------" >> resumo.txt
+udevadm info --query=all --name=/dev/sda | grep ID_SERIAL >> resumo.txt
+echo "---------------------------------------------------------" >> resumo.txt
+which gedit >> resumo.txt
+echo "---------------------------------------------------------" >> resumo.txt
+which git >> resumo.txt
+
